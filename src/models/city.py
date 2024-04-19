a840ba7dc5314d0967f6419f18777c36f11e2b64
Matthew <mathonich@yahoo.com>
2024-04-19 16:13:45 -0700

    City str and get functions

--- src/models/city.py
+++ src/models/city.py
@@ -1,5 +1,15 @@
 class City:
-    def __init__(self, name, longitude, latitude):
+    def __init__(self, name, longitude, latitude, connections = []):
         self.name = name
         self.longitude = longitude
         self.latitude = latitude
+        self.connections = connections
+
+    def __str__(self):
+        return self.name
+
+    def get_longitude(self):
+        return self.longitude
+
+    def get_latitude(self):
+        return self.latitude
