4ca7e897bb0293773e2f9bdf6bf2b16301b287c2
Matthew <mathonich@yahoo.com>
2024-04-19 16:36:14 -0700

    From string -> needs testing

--- src/models/city.py
+++ src/models/city.py
@@ -1,9 +1,10 @@
+import math
+
 class City:
-    def __init__(self, name, longitude, latitude, connections = []):
+    def __init__(self, name, longitude, latitude):
         self.name = name
         self.longitude = longitude
         self.latitude = latitude
-        self.connections = connections
 
     def __str__(self):
         return self.name
@@ -13,3 +14,20 @@ class City:
 
     def get_latitude(self):
         return self.latitude
+
+
+    @classmethod
+    def from_string(cls, string):
+        parts = string.split(" --> ")
+
+        city_info = parts[0].split()
+        name = city_info[0]
+        lat_degrees, lat_minutes, lat_seconds, lat_direction = city_info[1:5]
+        lon_degrees, lon_minutes, lon_seconds, lon_direction = city_info[5:]
+
+        latitude = float(lat_degrees) + float(lat_minutes) / 60 + float(lat_seconds) / 3600
+        longitude = float(lon_degrees) + float(lon_minutes) / 60 + float(lon_seconds) / 3600
+
+        print(name, longitude, latitude)
+        return cls(name, longitude, latitude)
+
