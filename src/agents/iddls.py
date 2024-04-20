02d218cc4242cbd4a041b554ca195e4cce36d1cf
Matthew <mathonich@yahoo.com>
2024-04-20 16:49:56 -0700

    IDDLS pseudocode

---
+++ src/agents/iddls.py
@@ -0,0 +1,33 @@
+from agents.agent import Agent
+from models.node import Node
+
+class IDDLSAgent(Agent):
+    """
+    Iterative Deepening Depth-Limited Search Agent
+    The following pseudocode is from Artificial Intelligence: A Modern Approach page 182
+
+    function ITERATIVE-DEEPENING-SEARCH(problem) returns a solution node or failure
+        for depth = 0 to infinity do
+            result <- DEPTH-LIMITED-SEARCH(problem, depth)
+            if result != cutoff then return result
+
+    function DEPTH-LIMITED-SEARCH(problem, limit) returns a solution node, failure, or cutoff
+        frontier <- a LIFO queue (stack), with NODE(problem.INITIAL) as an element
+        reached <- failure
+        while not IS-EMPTY(frontier) do
+            node <- POP(frontier)
+            if problem.IS-GOAL(node.STATE) then return node
+            if DEPTH(node) > limit then
+                result <- cutoff
+            else if not IS-CYCLE(node) then
+                for each child in EXPAND(problem, node) do
+                    add child to frontier
+        return result
+
+    """
+
+    def __init__(self):
+        """
+        Initializes the agent
+        """
+        super().__init__()
