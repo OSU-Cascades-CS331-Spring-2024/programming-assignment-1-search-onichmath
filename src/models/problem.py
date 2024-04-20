d4c66349f2b04271bf6f2cef4ef74dbddebb5aed
Matthew <mathonich@yahoo.com>
2024-04-20 14:12:19 -0700

    Added problem to fit expand pseudocode from book

--- src/models/problem.py
+++ src/models/problem.py
@@ -1,8 +1,32 @@
-class Problem:
-    def __init__(self, initial_state, goal_state):
+class Problem():
+    def __init__(self, start_city_name, goal_city_name, map):
         """
-        Initializes a problem with an initial state and a goal state
+        Initializes a problem with a start and goal state
         """
-        self.initial_state = initial_state
-        self.goal_state = goal_state
+        self.start_state = start_city_name
+        self.goal_state = goal_city_name
+        self.map = map
 
+    def actions(self, state):
+        """
+        Returns the possible actions from the given state
+        """
+        return state.get_connections()
+
+    def goal_test(self, state):
+        """
+        Returns whether the given state is the goal state
+        """
+        return state.name == self.goal_state
+
+    def result(self, state, action):
+        """
+        Returns the state that results from the given action
+        """
+        return action
+
+    def action_cost(self, state, state_prime):
+        """
+        Returns the cost of the given action
+        """
+        return state.get_cost(state_prime)
