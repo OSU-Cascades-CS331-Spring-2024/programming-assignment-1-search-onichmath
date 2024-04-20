fa4517f472fda6dd158145b5adf346bf20ad26f7
Matthew <mathonich@yahoo.com>
2024-04-20 13:37:13 -0700

    BFS spaces

--- src/agents/bfs.py
+++ src/agents/bfs.py
@@ -7,16 +7,21 @@ class BFS(Agent):
 
     function BREADTH-FIRST-SEARCH(problem) returns a solution node or failure
         node <- NODE(problem.INITIAL)
+
         if problem.IS-GOAL(node.STATE) then return node
+
         frontier <- a FIFO queue, with node as an element
         reached <- {problem.INITIAL}
+
         while not EMPTY(frontier) do
             node <- POP(frontier)
+
             for each child in EXPAND(problem, node) do
                 s <- child.STATE
                 if problem.IS-GOAL(s) then return child
                 if s not in reached then
                     add s to reached
                     add child to frontier
+
         return failure
     """
