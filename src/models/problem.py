56a88e098b0fb46f95dd0ecdb119ae0c9f5a2cf6
Matthew <mathonich@yahoo.com>
2024-04-20 13:44:25 -0700

    Problem class

---
+++ src/models/problem.py
@@ -0,0 +1,8 @@
+class Problem:
+    def __init__(self, initial_state, goal_state):
+        """
+        Initializes a problem with an initial state and a goal state
+        """
+        self.initial_state = initial_state
+        self.goal_state = goal_state
+
