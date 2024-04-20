63088557f2077bb83cc7ac374b5f4588aed6ef95
Matthew <mathonich@yahoo.com>
2024-04-19 17:31:18 -0700

    Map: lines from file name

--- src/models/map.py
+++ src/models/map.py
@@ -1,2 +1,17 @@
 class Map:
-    pass
+    def __init__(self, cities):
+        """
+        Initializes a map with cities
+        """
+        self.cities = cities
+
+    @classmethod
+    def from_file(cls, file_name):
+        file_path = "../../map_data/" + file_name + ".txt"
+        with open(file_path, "r") as f:
+            lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        print(lines)
+
+if __name__ == "__main__":
+    Map.from_file("france")
