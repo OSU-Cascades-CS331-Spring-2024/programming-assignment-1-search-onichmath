40ec159dd48123e43d3a4656dcc8fafe38488422
Matthew <mathonich@yahoo.com>
2024-04-20 13:16:13 -0700

    Removed search methods

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -14,27 +14,3 @@ class Agent:
         self.explored = 0 
         self.expanded = 0 
         self.maintained = 0
-
-    def bfs(self, start, goal):
-        """
-        Breadth-first search
-        """
-        pass
-
-    def iddls(self, start, goal):
-        """
-        Iterative deepening depth-limited search
-        """
-        pass
-
-    def ucs(self, start, goal):
-        """
-        Uniform cost search
-        """
-        pass
-
-    def astar(self, start, goal):
-        """
-        A* search
-        """
-        pass
