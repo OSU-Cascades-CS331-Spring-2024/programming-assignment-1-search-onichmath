dc9bb61a018f2c561aaf4a98a7fef48fe5fafe1b
Matthew <mathonich@yahoo.com>
2024-04-19 17:48:38 -0700

    Map: __str__ and __repr__ as well as mapping cities

--- src/models/map.py
+++ src/models/map.py
@@ -8,6 +8,18 @@ class Map:
         """
         self.cities = cities
 
+    def __str__(self):
+        """
+        Returns the names of the cities in the map
+        """
+        return str([str(city) for city in self.cities])
+
+    def __repr__(self):
+        """
+        Returns a string representation of the map
+        """
+        return f"Map(cities={self.cities})"
+
     @staticmethod 
     def map_lines_from_filename(file_name):
         file_path = "../../map_data/" + file_name + ".txt"
@@ -23,8 +35,10 @@ class Map:
     @classmethod
     def from_file(cls, file_name):
         lines = cls.map_lines_from_filename(file_name)
-        cities = {City.from_string(line) for line in lines}
-        return cls(cities)
+        cities = [City.from_string(line) for line in lines]
+        cities_mapped = {city.get_name():city for city in cities}
+        return cls(cities_mapped)
 
 if __name__ == "__main__":
     map = Map.from_file("france")
+    print(map)
