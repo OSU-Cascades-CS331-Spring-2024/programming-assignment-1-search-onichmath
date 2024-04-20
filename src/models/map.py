f995a372c32dceec1422984ef2f289dfe85482d3
Matthew <mathonich@yahoo.com>
2024-04-19 17:36:45 -0700

    Lines from filename

--- src/models/map.py
+++ src/models/map.py
@@ -1,3 +1,5 @@
+import sys
+
 class Map:
     def __init__(self, cities):
         """
@@ -5,13 +7,21 @@ class Map:
         """
         self.cities = cities
 
+    @staticmethod 
+    def map_lines_from_filename(file_name):
+        file_path = "../../map_data/" + file_name + ".txt"
+        try:
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+        except FileNotFoundError:
+            print(f"Failed to open {file_path}")
+            sys.exit(1)
+        lines = [line.strip() for line in lines]
+        return lines
+
     @classmethod
     def from_file(cls, file_name):
-        file_path = "../../map_data/" + file_name + ".txt"
-        with open(file_path, "r") as f:
-            lines = f.readlines()
-        lines = [line.strip() for line in lines]
-        print(lines)
+        pass
 
 if __name__ == "__main__":
     Map.from_file("france")
