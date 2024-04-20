26f7a86268c90c50c5d49a69b79b96301d90c8d2
Matthew <mathonich@yahoo.com>
2024-04-19 21:27:27 -0700

    List maps

--- src/help.py
+++ src/help.py
@@ -1,9 +1,16 @@
+import os
 
 def usage():
     print("Usage: python main.py -M <map_name> -A <start_city> -B <end_city> -S <search_algorithm>")
 
+def list_maps():
+    maps = os.listdir("../map_data")
+    # maps = [map.split(".")[0] for map in maps]
+    return maps
+
 def help():
     usage()
+    list_maps()
     print("Search algorithms: bfs, iddls, ucs, astar")
 
 if __name__ == "__main__":
