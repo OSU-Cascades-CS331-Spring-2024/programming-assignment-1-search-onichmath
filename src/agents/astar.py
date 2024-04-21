1775990654a136939ad55078977ed2673c96c820
Matthew <mathonich@yahoo.com>
2024-04-20 17:24:05 -0700

    A* template

---
+++ src/agents/astar.py
@@ -0,0 +1,16 @@
+from agents.agent import Agent
+from models.node import Node
+
+class AStarAgent(Agent):
+    """
+    A* Search Agent
+
+    TODO: Find pseudocode for A* in book or use wikipedia
+    """
+
+    def __init__(self):
+        """
+        Initializes the agent
+        """
+        super().__init__()
+
