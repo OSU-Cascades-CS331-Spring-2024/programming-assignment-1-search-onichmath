03f9d491d4ff7e73f205c140b6a9930e207dbe96
Matthew <mathonich@yahoo.com>
2024-04-20 17:27:47 -0700

    Main script

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import usage
 import sys
 from agents.bfs import BFSAgent
+from agents.ucs import UCSAgent
+from agents.iddls import IDDLSAgent
 from models.problem import Problem
 from models.map import Map
 from models.city import City
@@ -11,6 +13,16 @@ if __name__ == "__main__":
     start_city = map.cities["brest"]
     goal_city = map.cities["nice"]
     problem = Problem(start_city, goal_city, map)
-    agent = BFSAgent()
-    solution = agent.search(problem)
-    print(agent)
+
+    ucs_agent = UCSAgent()
+    bfs_agent = BFSAgent()
+    iddls_agent = IDDLSAgent()
+
+    ucs_solution = ucs_agent.search(problem)
+    bfs_solution = bfs_agent.search(problem)
+    iddls_solution = iddls_agent.search(problem)
+
+    print(f"{problem}\n")
+    print(f"{bfs_agent}\n")
+    print(f"{ucs_agent}\n")
+    print(f"{iddls_agent}\n")
