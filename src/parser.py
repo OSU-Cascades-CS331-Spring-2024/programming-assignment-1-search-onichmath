16c1ebdec37c0cbdea15830a9c2f743d0469c80c
Matthew <mathonich@yahoo.com>
2024-04-22 16:47:36 -0700

    Getting arguments from parser

--- src/parser.py
+++ src/parser.py
@@ -10,8 +10,8 @@ class ArgParser:
         self.parser.add_argument("-B", "-b", help="End city")
 
 
-        algorithm_choices = ["bfs", "dfs", "ucs", "iddls", "astar"]
-        self.parser.add_argument("-S", "-s", choices=algorithm_choices, help=f"Search algorithm to use. Options are: bfs, dfs, ucs, iddls, astar")
+        algorithm_choices = ["bfs", "dfs", "ucs", "dls", "astar"]
+        self.parser.add_argument("-S", "-s", choices=algorithm_choices, help=f"Search algorithm to use. Options are: bfs, dfs, ucs, dls, astar")
 
         map_choices = get_maps()
         self.parser.add_argument("-M", "-m", choices=map_choices, help=f"Map file name w/o extension. Options are {map_choices}")
@@ -19,6 +19,33 @@ class ArgParser:
         heuristic_choices = ["euclidean", "haversine"] 
         self.parser.add_argument("-H", "--heuristic", choices=heuristic_choices, help="Heuristic to use for A* search. Options are: euclidean, haversine")
 
+        self.args = self.parser.parse_args()
 
-    def parse_args(self, args=None):
-        return self.parser.parse_args(args)
+    def get_cities(self):
+        if self.args.A and self.args.B:
+            return [[self.args.A, self.args.B]]
+        return [["brest", "nice"],
+                ["montpellier", "calais"],
+                ["strasbourg", "bordeaux"],
+                ["paris", "grenoble"],
+                ["grenoble", "paris"],
+                ["brest", "grenoble"],
+                ["grenoble", "brest"],
+                ["nice", "nantes"],
+                ["caen", "strasbourg"]]
+
+    def get_algorithm(self):
+        if self.args.S:
+            return self.args.S 
+        return [
+            "bfs",
+            "ucs",
+            "dls",
+            "astar"
+                ]
+
+    def get_map(self):
+        if self.args.M:
+            return self.args.M
+        return "france"
+
