fb4cbe1d3d7dbb3af454d4b0f29e866a3cf76bc6
Matthew <mathonich@yahoo.com>
2024-04-22 15:59:19 -0700

    XYZ heuristic

--- src/models/problem.py
+++ src/models/problem.py
@@ -1,7 +1,10 @@
 from models.city import City
+import math
 from models.node import Node
 
 class Problem():
+    polar_earth_radius_km = 6357.0
+
     def __init__(self, start_city_name, goal_city_name, map):
         """
         Initializes a problem with a start and goal state
@@ -46,3 +49,21 @@ class Problem():
             s_prime = self.map.get_city(self.result(s, action))
             c = node.path_cost + self.action_cost(s, s_prime)
             yield Node(s_prime, c, node.path + [s_prime.name])
+
+    def heuristic_xyz(self, state):
+        """
+        Returns the heuristic value for the given state
+        """
+        x1, y1, z1 = self.polar_to_cartesian_km(state.longitude, state.latitude)
+        x2, y2, z2 = self.polar_to_cartesian_km(self.goal_state.longitude, self.goal_state.latitude)
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2) ** 0.5
+
+    def polar_to_cartesian_km(self, longitude, latitude):
+        """
+        Converts spherical polar coordinates to cartesian coordinates, assuming the Earth is a sphere
+        https://stackoverflow.com/questions/1185408/converting-from-longitude-latitude-to-cartesian-coordinates
+        """
+        x = math.cos(math.radians(latitude)) * math.cos(math.radians(longitude)) * self.polar_earth_radius_km
+        y = math.cos(math.radians(latitude)) * math.sin(math.radians(longitude)) * self.polar_earth_radius_km
+        z = math.sin(math.radians(latitude)) * self.polar_earth_radius_km
+        return x, y, z
